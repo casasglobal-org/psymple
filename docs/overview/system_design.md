fef1a0a0df3870e02f664a477fccb90cdce85768
George Simmons <g.simmons@idems.international>
2024-09-13 10:35:13 +0100

    examples update

--- docs/overview/system_design.md
+++ docs/overview/system_design.md
@@ -1,6 +1,6 @@
 # System design
 
-`psymple` is built to maximise accessibility and reusability by incorportating the following principles at the core of the design.
+`psymple` is built to maximise accessibility and reusability by incorporating the following principles at the core of the design.
 
 ## 1. Modular components
 
@@ -9,7 +9,7 @@ Systems of any complexity can be modelled by versatile building blocks called "p
 These objects can be linked by "wires" which formally represent the flow of resource or information around the system. This provides a degree of separation from the modeller to the dynamically-created system equations, which provides:
 
 1. **Faceted approach**: each component can be conceived, built and tested in isolation from the system.
-2. **Diagramatic interface**: complex systems are often visualised through diagrams representing the interconnectivity. The interface to `psymple` is a codified approach to building these diagrams.
+2. **Diagrammatic interface**: complex systems are often visualised through diagrams representing the interconnectivity. The interface to `psymple` is a codified approach to building these diagrams.
 3. **Natural interaction**: connections from other parts in the system can override default behaviours and capture information or composite variable dynamics automatically.
 
 ## 2. Symbolic systems
@@ -17,12 +17,12 @@ These objects can be linked by "wires" which formally represent the flow of reso
 The python mathematics library [sympy](https://www.sympy.org/en/index.html) allows for system models to be built, stored and manipulated symbolically. This feature is designed to provide:
 
 1. **Clarity**: any system component can produce its own system of equations for easy inspection and checking.
-2. **Dependanbility**: models built using computer algebra removes mistakes in combining multiple components.
+2. **Dependability**: models built using computer algebra removes mistakes in combining multiple components.
 2. **Efficiency**: systems built from many parts produce complex equations which can be automatically simplified to aid simulation.
 
 ## 3. Data-first design
 
-Systems are built and stored using a flexible `json` data model for its main objects which allows them to be built from, or dismantled into, wide-ranging formats. This is desgined to provide:
+Systems are built and stored using a flexible `json` data model for its main objects which allows them to be built from, or dismantled into, wide-ranging formats. This is designed to provide:
 
 1. **Collaborative modelling**: varied domain experts with different technical languages or interpretations of concepts can develop and interact with different interfaces to the core data format. [Read more about collaborative modelling.](https://topos.institute/collaborative-modelling)
 2. **Traceability**: a data-centric focus provides a more natural setting to integrate data and assumption sources into the platform for clear and trustable model creation.
---
+++ examples/mixing_problems/2-multiple_tanks.py
@@ -0,0 +1,172 @@
+import pathlib
+import sys
+
+from sympy import Symbol
+
+sys.path.insert(0, str(pathlib.Path(__file__).parents[2]))
+
+from psymple.build import (
+    CompositePortedObject,
+    FunctionalPortedObject,
+    VariablePortedObject,
+    System,
+)
+
+"""
+In the previous example, we considered a mixing problem with a single tank, which had an external 
+flow in and an external flow out. Now we consider a mixing problem consisting of two or more
+tanks, each of which can have external flows in and out in the same manner as for the single tank,
+and which furthermore can have flows to and from other tanks in the system.
+
+It will help to first specify the system data. In this case, we will create a system with \( n > 1 \)
+tanks, such that every tank has a flow in and flow out, plus a flow to and from every other tank.
+The following data can be freely altered to reduce the external flows and connections, if desired.
+"""
+
+# Number of tanks
+n = 2
+
+# List of tanks created
+tanks_id = [i for i in range(1, n + 1)]
+
+# List of pipes between tanks eg. (1,2) is a pipe from tank 1 to tank 2
+link_pipes_id = [(i, j) for i in range(1, n + 1) for j in range(1, n + 1) if j != i]
+
+# List of tanks with an external pipe in
+pipes_in_id = [1, 2]
+
+# List of tanks with an external pipe out
+pipes_out_id = [1, 2]
+
+"""
+If you followed through the single tank example, the following lists of pipes in and out are created
+in exactly the same way as there. We create pipes in for every tank in `pipes_in_id` and pipes out
+for every tank in `pipes_out_id`.
+"""
+
+pipes_in = [
+    VariablePortedObject(
+        name=f"in_{i}",
+        assignments=[
+            ("Q_in", "rate * conc"),
+            ("V_in", "rate"),
+        ],
+    )
+    for i in pipes_in_id
+]
+
+pipes_out = [
+    VariablePortedObject(
+        name=f"out_{i}",
+        assignments=[("Q_out", "-rate * Q_out / V_out"), ("V_out", "-rate")],
+    )
+    for i in pipes_out_id
+]
+
+"""
+Next, we need to define connector pipes. These have four variables:
+
+- `Q_in`, the amount of salt entering the pipe from a tank,
+- `Q_out`, the amount of salt flowing into the next tank. Note that this has the same concentration
+    as for `Q_in`,
+- `V_in`, the volume of water entering the pipe from the tank,
+- `V_out`, the volume of water exiting the pipe into the next tank.
+
+We create a connector pipe for every pair in `link_pipes_id`.
+"""
+
+connectors = [
+    VariablePortedObject(
+        name=f"pipe_{i}_{j}",
+        assignments=[
+            ("Q_in", "- rate * Q_in / V_in"),
+            ("Q_out", "rate * Q_in / V_in"),
+            ("V_in", "-rate"),
+            ("V_out", "rate"),
+        ],
+    )
+    for i, j in link_pipes_id
+]
+
+"""
+Now we just need to define our system of tanks. Compared to the single tank example, 
+the children, variable ports and input ports are exactly the same (just one for each tank, 
+if it was specified), except for aditionally the specification of the flow rates `rate_i_j` 
+of the connector pipes as additional input ports.
+
+The directed wires simply connect the rates at the input ports to the correct pipes.
+
+The only thing which becomes more complicated is the aggregation of the variables. Each tank `i`,
+and therefore each variable `Q_i` (and `V_i`), can have:
+
+- External in-flows if `i in pipes_in_id`,
+- External out-flows if `i in pipes_out_id`,
+- A flow to tank `j` for every `j` such that `(i,j) in link_pipes_id`,
+- A flow from tank `j` for every `j` such that `(j,i) in link_pipes_id`.
+
+The variable_wires data simply aggregates all these variables to `Q_i` (and `V_i`).
+"""
+
+tanks = CompositePortedObject(
+    name="tanks",
+    children=pipes_in + pipes_out + connectors,
+    variable_ports=[f"Q_{i}" for i in tanks_id] + [f"V_{i}" for i in tanks_id],
+    input_ports=[f"rate_{i}_{j}" for i, j in link_pipes_id]
+    + [f"conc_in_{i}" for i in pipes_in_id]
+    + [f"rate_in_{i}" for i in pipes_in_id]
+    + [f"rate_out_{i}" for i in pipes_out_id],
+    directed_wires=[(f"rate_{i}_{j}", f"pipe_{i}_{j}.rate") for i, j in link_pipes_id]
+    + [(f"conc_in_{i}", f"in_{i}.conc") for i in pipes_in_id]
+    + [(f"rate_out_{i}", f"out_{i}.rate") for i in pipes_out_id]
+    + [(f"rate_in_{i}", f"in_{i}.rate") for i in pipes_in_id],
+    variable_wires=[
+        (
+            ([f"in_{i}.Q_in"] if i in pipes_in_id else [])
+            + ([f"out_{i}.Q_out"] if i in pipes_out_id else [])
+            + [f"pipe_{j}_{i}.Q_out" for j in tanks_id if j != i]
+            + [f"pipe_{i}_{j}.Q_in" for j in tanks_id if j != i],
+            f"Q_{i}",
+        )
+        for i in tanks_id
+    ]
+    + [
+        (
+            ([f"in_{i}.V_in"] if i in pipes_in_id else [])
+            + ([f"out_{i}.V_out"] if i in pipes_out_id else [])
+            + [f"pipe_{j}_{i}.V_out" for j in tanks_id if j != i]
+            + [f"pipe_{i}_{j}.V_in" for j in tanks_id if j != i],
+            f"V_{i}",
+        )
+        for i in tanks_id
+    ],
+)
+
+"""
+And that's it!
+"""
+
+S = System(tanks)
+S.compile()
+
+S.set_parameters(
+    {
+        "rate_1_2": 3,
+        "rate_2_1": 10,
+        "rate_in_1": "Piecewise((4, T<50), (0, True))",
+        "rate_in_2": 7,
+        "conc_in_1": 1 / 2,
+        "conc_in_2": 0,
+        "rate_out_1": 11,
+        "rate_out_2": 0
+    }
+)
+
+print(S)
+
+sym = S.create_simulation(
+    solver="cts", initial_values={"Q_1": 20, "Q_2": 80, "V_1": 800, "V_2": 1000}
+)
+
+sym.simulate(100, n_steps=100)
+
+sym.plot_solution()
